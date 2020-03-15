a5059a0ab925ff2e5f2ecffac8b5b03ce344d538
YujiYashima <pedro.yashima@etec.sp.gov.br>
2020-03-15 17:40:02 -0300

    iRock

--- interaçao.js
+++ interaçao.js
@@ -1,18 +1,30 @@
+var userName;
+
 function carregar() {
 
     alert("Hello, eu sou a rocha de estimação");
     
 }
 
-function touchRock() {
-
-    var userName = prompt("Qual o seu nome?");
+function clique() {
 
     if (userName) {
-        
-        alert("É um preazer te conhecer "+userName+".");
-        document.getElementById("rockImg").src = "irock smile.png";
-        
+    
+        alert("Eu gosto de atençao, "+userName+".");
+
+    }else{
+
+        userName = prompt("Qual seu nome?", "Digite seu nome aqui!");
+
+        if (userName) {
+
+            alert("Przer em conhecer voce, "+userName+".");
+            
+        }
+
     }
+
+    document.getElementById("rockImg").src = "irock smile.png";
+    setTimeout("document.getElementbyId('rockImg').src = 'irock.png';", 1*60*1000);
     
 }
